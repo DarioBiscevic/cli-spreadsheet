e00f29c4b64201817d2ce9dcb0f3c4b09e8b14ea
DarioBiscevic <dario.biscevic03@gmail.com>
2020-11-25 21:56:26 +0100

    Parser & added startup and lib folders

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+
+mod lib;
+mod startup;
+
 fn main() {
-    println!("Hello, world!");
+    println!("cli-spreadsheet");
+
+    match parse_args(){
+        Settings::Load   => ,
+        Settings::Create => ,
+        _                => println!("Argument error"),
+    }
+}
+
+enum Settings{
+    Load(String),   //Load <filename>
+    Create(String), //Create <filename>
+}
+
+
+//TODO: improve parser
+fn parse_args() -> Result<Settings, ()>{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        Err(())
+    }else{
+        if args[1] == "load"{
+            Ok(Settings::Load(args[2].clone())
+        }else if args[1] == "create"{
+            Ok(Settings::Create(args[2].clone())
+        }else{
+            Err(())
+        }
+    }
 }
