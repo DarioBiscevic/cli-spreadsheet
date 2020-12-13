9b1deaa2c33511cb159efc97883cbcbcbb1d1207
DarioBiscevic <dario.biscevic03@gmail.com>
2020-12-13 19:18:48 +0100

    Defined Cell structure

--- src/spreadsheet/cell/mod.rs
+++ src/spreadsheet/cell/mod.rs
@@ -1,11 +1,19 @@
 pub struct Cell{
     expression: String,
+    value:      ValueType
 }
 
 impl Cell{
     pub fn new() -> Self{
         Cell{
-            expression: String::new()
+            expression: String::new(),
+            value:      ValueType::Empty
         }
     }
+}
+
+enum ValueType{
+    Literal(String),
+    Numeric(f64),
+    Empty
 }
